cd4ba24b4ab4af8fc6023af40919026a067b96ab
Aixa Rodriguez <aixa.201@gmail.com>
2021-07-09 21:31:17 -0300

    Cuarta opcion

--- Pruebas de Inventario.py
+++ Pruebas de Inventario.py
@@ -1,7 +1,8 @@
+from os import lseek
 import tkinter
 from tkinter import *
 from tkinter import scrolledtext
-import tkinter as tk
+from tkinter.ttk import *
 
 #Variables necesarias
 
@@ -64,9 +65,9 @@ def help_window():
 
 inicio = Tk()
 inicio.wm_title("Inventario de filamentos")
-inicio.geometry('310x250')
+inicio.geometry('350x300')
 bienvenida = Label(inicio, text="Bienvenido al inventario de filamentos",font = ("Agency FB", 20))
-bienvenida.grid(column=0,row=1)
+bienvenida.grid(column=0, row=1, padx=10, pady=10)
 menu = Menu(inicio)
 new_item = Menu(menu, tearoff=0)
 menu.add_command(label='Ayuda', command=help_window)
@@ -76,7 +77,7 @@ inicio.config(menu=menu)
 #comando default para las que no estan definidas
 def click():
     confirmacion_eleccion = Label(inicio,text="Elegir una de las siguientes opciones:")
-    confirmacion_eleccion.grid(column=0,row=2)
+    confirmacion_eleccion.grid(column=0, row=2, pady=1, padx=1 )
     confirmacion_eleccion.config(text="Seleccionado!")
 
 
@@ -90,10 +91,10 @@ def primer_opcion():
     ventana_filamentos_en_Stock.geometry("300x150")
     ventana_filamentos_en_Stock.wm_title("Filamentos en Stock")
     contenido = scrolledtext.ScrolledText(ventana_filamentos_en_Stock, width=25,height=5,font = ("Century Gothic", 15))
-    contenido.grid(column=0,row=0)
+    contenido.grid(column=0, row=0, padx=10, pady=10)
     contenido.insert(INSERT, "\n".join(filamentos_en_stock))
     volver = Button(ventana_filamentos_en_Stock,text="Volver",command=ventana_filamentos_en_Stock.destroy)
-    volver.grid(column=0,row=9)
+    volver.grid(column=0, row=9, pady=10, padx=10)
 
 #Ventana que se abre con la segunda opcion
 def segunda_opcion():
@@ -105,23 +106,75 @@ def segunda_opcion():
     ventana_colores.geometry("300x150")
     ventana_colores.wm_title("Colores en Stock")
     contenido = scrolledtext.ScrolledText(ventana_colores, width=25,height=5,font = ("Century Gothic", 15))
-    contenido.grid(column=0,row=0)
+    contenido.grid(column=0, row=0, pady=10, padx=10)
     contenido.insert(INSERT, "\n".join(colores))
     volver = Button(ventana_colores,text="Volver",command=ventana_colores.destroy)
-    volver.grid(column=0,row=9)
+    volver.grid(column=0, row=9, padx=10, pady=10)
+
+def tercer_opcion():
+    global ventana_materiales
+    ventana_materiales = tkinter.Toplevel()
+    menu = Menu(ventana_materiales)
+    menu.add_command(label='Ayuda', command=help_window)
+    ventana_materiales.config(menu=menu)
+    ventana_materiales.geometry("300x150")
+    ventana_materiales.wm_title("Materiales en Stock")
+    contenido = scrolledtext.ScrolledText(ventana_materiales, width=25,height=5,font = ("Century Gothic", 15))
+    contenido.grid(column=0, row=0, pady=10, padx=10)
+    contenido.insert(INSERT, "\n".join(materiales))
+    volver = Button(ventana_materiales,text="Volver",command=ventana_materiales.destroy)
+    volver.grid(column=0, row=9, padx=10, pady=10)
+
+def cuarta_opcion():
+    global ventana_agregar
+    ventana_agregar = tkinter.Toplevel()
+    menu = Menu(ventana_agregar)
+    menu.add_command(label="Ayuda", command=help_window)
+    ventana_agregar.config(menu=menu)
+    ventana_agregar.geometry("200x400")
+    ventana_agregar.wm_title("Agregar filamento")
+    titulo = Label(ventana_agregar, text="Seleccionar los siguientes datos")
+    titulo.grid(column=0, row=0,padx=1)
+    titulo_marca = Label(ventana_agregar, text="Marca:")
+    titulo_marca.grid(column=0, row=1, padx=5)
+    lista_marcas = Combobox(ventana_agregar)
+    lista_marcas['values']= ("Seleccionar","PrintALot", "Grillon3", "MakerParts", "PlastAr", "GST")
+    lista_marcas.current(0)
+    lista_marcas.config(state="readonly")
+    lista_marcas.grid(column=0, row=2, padx=5)
+    titulo_material = Label(ventana_agregar, text="Material:")
+    titulo_material.grid(column=0, row=3, padx=5)
+    lista_material = Combobox(ventana_agregar)
+    lista_material['values']= ("Seleccionar","PLA", "PETG", "ABS", "FLEX")
+    lista_material.current(0)
+    lista_material.config(state="readonly")
+    lista_material.grid(column=0, row=4, padx=5)
+    titulo_color = Label(ventana_agregar, text="Color:")
+    titulo_color.grid(column=0, row=5, padx=5)
+    entrada_color = Entry(ventana_agregar)
+    entrada_color.grid(column=0, row=6, padx=5)
+    titulo_tipo_color = Label(ventana_agregar, text="Tipo de Color:")
+    titulo_tipo_color.grid(column=0, row=7, padx=5)
+    lista_tipo_color = Combobox(ventana_agregar)
+    lista_tipo_color['values']= ("Seleccionar","Comun", "Translucido", "Especial", "UV", "Glitter", "Bicolor", "Pastel", "Metalizado")
+    lista_tipo_color.current(0)
+    lista_tipo_color.config(state="readonly")
+    lista_tipo_color.grid(column=0, row=8, padx=5)
+    
+
 
 
 opcion1 = Button(inicio,text="Filamentos en Stock(Por codigo)",command=primer_opcion)
-opcion1.grid(column=0,row=3)
+opcion1.grid(column=0,row=3, pady=5)
 opcion2 = Button(inicio,text="Colores en Stock",command=segunda_opcion)
-opcion2.grid(column=0,row=4)
-opcion3 = Button(inicio,text="Materiales en Stock",command=click)
-opcion3.grid(column=0,row=5)
-opcion4 = Button(inicio,text="Agregar un filamento",command=click)
-opcion4.grid(column=0,row=6)
+opcion2.grid(column=0,row=4, pady=5)
+opcion3 = Button(inicio,text="Materiales en Stock",command=tercer_opcion)
+opcion3.grid(column=0,row=5, pady=5)
+opcion4 = Button(inicio,text="Agregar un filamento",command=cuarta_opcion)
+opcion4.grid(column=0,row=6, pady=5)
 opcion5 = Button(inicio,text="Actualizar datos de un filamento",command=click)
-opcion5.grid(column=0,row=7)
+opcion5.grid(column=0,row=7, pady=5)
 opcion6 = Button(inicio,text="Salir",command=exit)
-opcion6.grid(column=0,row=9)
+opcion6.grid(column=0,row=9, pady=5)
 
 inicio.mainloop()
